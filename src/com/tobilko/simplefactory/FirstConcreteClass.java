e561eb92ef6a0d9c672450e40200cc3fd0f08786
Andrew Tobilko <andrew.tobilko@gmail.com>
2015-11-24 21:26:58 +0200

    Simple Factory

---
+++ src/com/tobilko/simplefactory/FirstConcreteClass.java
@@ -0,0 +1,4 @@
+package com.tobilko.simplefactory;
+
+public class FirstConcreteClass {
+}
---
+++ src/com/tobilko/simplefactory/SecondConcreteClass.java
@@ -0,0 +1,5 @@
+package com.tobilko.simplefactory;
+
+public class SecondConcreteClass implements Interface {
+
+}
