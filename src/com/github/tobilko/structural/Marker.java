fe216a21a6ad3cfbd82b5aca84f76292a2a05e02
Andrew Tobilko <andrew.tobilko@gmail.com>
2016-11-08 23:58:25 +0200

    the marker pattern

---
+++ src/com/github/tobilko/structural/Marker.java
@@ -0,0 +1,12 @@
+package com.github.tobilko.structural;
+
+/**
+ * A marker interface to associate a class with meta-information.
+ *
+ * The limitation is that a class should implement it to be a marker.
+ * Consider annotations to avoid contract compliance by a class and all its subtypes.
+ *
+ * Created by Andrew Tobilko on 11/8/2016.
+ *
+ */
+public class Marker {}
